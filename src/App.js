839de54c6e1e8a8bbbf59d605c16d548714d29b2
Leandro Vieira <leandrown@outlook.com>
2020-01-15 15:04:33 -0300

    Conceitos do React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+
+// Componente: Bloco isolado de HTML, CSS e JS, o qual nao interfere no restante da aplicacao
+// Sempre com primeira letra maiuscula, o componente eh uma funcao que retorna algum
+// conteudo HTML, CSS ou mesmo JavaScript
+// Como regra, o React usa sempre UM componente por arquivo
+
+// Propriedade: Informacoes que um componente PAI passa para o componente FILHO
+// Caracteristica do componente (atributos em HTML)
+// NOTA: quando mais de um componente eh inserido dentro do render() deve-se usar um container,
+// geralmente usando uma tag <div></div>, porem isso pode atrapalhar a estilizacao em alguns casos.
+// Como alternativa, pode ser usado o conceito de fragment, usando tags vazias ' <></> '
+
+// Estado: Informacoes mantidas pelo componente (Lembrar: conceito de imutabilidade)
+// Informacao que o componente vai manipular, Nunca altera uma informacao do estado diretamente.
+// Usamos sempre uma funcao auxiliar que vai dar a opcao para alterar o estado.
 
 function App() {
+   const [counter, setCounter] = useState(0);
+
+   function incrementCounter() {
+      setCounter(counter + 1);
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+         <h1>Contador: {counter}</h1>
+         <button onClick={incrementCounter}>Incrementar</button>
+      </>
   );
 }
 
